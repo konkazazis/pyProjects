921768eafafa5a4633b5493e8effdd914c28b6d6
Konstantinos <kazaziskonstantinos@gmail.com>
2023-05-30 11:15:55 +0300

    pack doesnt work well, need grid manager

--- pyNew.py
+++ pyNew.py
@@ -2,14 +2,27 @@ from tkinter import *
 import tkinter as tk
 from PIL import ImageTk, Image
 
-
+# create window
 window = tk.Tk()
 
+# create frames and position side by side
 frame1 = tk.Frame(master=window, width=400, height=700, bg="light grey")
 frame1.pack(fill=tk.Y, side=tk.LEFT)
 
 frame2 = tk.Frame(master=window, width=800, height=700, bg="white")
 frame2.pack(fill=tk.Y, side=tk.LEFT)
+
+frame3 = tk.Frame(master=frame1, width=400, height=200, bg="blue")
+frame3.pack(expand=True, side=tk.TOP, fill=NONE)
+
+# open and resize image to fit the window
+image = Image.open("Roasted_coffee_beans.jpg")
+resize_image = image.resize((800, 700))
+img = ImageTk.PhotoImage(resize_image)
+
+# Create a Label Widget to display the text or Image
+label = Label(frame2, image = img)
+label.pack()
 
 
 
