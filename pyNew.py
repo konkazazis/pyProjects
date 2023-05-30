aee497968cd739e9e8d54fba729ae7336c8511bd
Konstantinos <kazaziskonstantinos@gmail.com>
2023-05-30 14:37:25 +0300

    good

--- pyNew.py
+++ pyNew.py
@@ -12,12 +12,15 @@ frame1.pack(fill=tk.Y, side=tk.LEFT)
 frame2 = tk.Frame(master=window, width=800, height=700, bg="white")
 frame2.pack(fill=tk.Y, side=tk.LEFT)
 
-frame3 = tk.Frame(master=frame1, width=400, height=200, bg="blue")
-frame3.pack(expand=True, side=tk.TOP, fill=NONE)
+frame3 = tk.Frame(master=frame1, width=400, height=100, bg="dark grey")
+frame3.grid(row=0, column=0, padx=10, pady=10)
+
+frame3 = tk.Frame(master=frame1, width=400, height=400, bg="dark grey")
+frame3.grid(row=1, column=0, padx=10, pady=10)
 
 # open and resize image to fit the window
 image = Image.open("Roasted_coffee_beans.jpg")
-resize_image = image.resize((800, 700))
+resize_image = image.resize((800, 600))
 img = ImageTk.PhotoImage(resize_image)
 
 # Create a Label Widget to display the text or Image
